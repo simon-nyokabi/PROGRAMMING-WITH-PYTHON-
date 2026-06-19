563fd5ec444c5af80a856f929c79c2653e4a9812
SIMON NYOKABI <simonnyokabi76@gmail.com>
2026-06-19 12:40:38 +0300

    USING SIMPLE FILE OPENINGS TO SAVE OUR BOOKS

--- 16_OOP_library.py
+++ 16_OOP_library.py
@@ -5,18 +5,24 @@ class Book:
         self.is_available = True
     def checkout_book(self):
         if self.is_available:
-            print(f"📖 Success! You checked out '{self.title}'.")
+            print(f"Success! You checked out '{self.title}'.")
             self.is_available = False
         else:
-            print(f"❌ Sorry, '{self.title}' is already checked out!")
+            print(f"Sorry, '{self.title}' is already checked out!")
         
     def return_book(self):
         if not self.is_available:
-            print(f"🔄 Success! '{self.title}' has been returned to the shelf.")
+            print(f"Success! '{self.title}' has been returned to the shelf.")
             self.is_available = True
         else:
-            print(f"🤔 '{self.title}' is already sitting on the shelf.")
+            print(f"'{self.title}' is already sitting on the shelf.")
 book_1 = Book("The Hobbit", "J.R.R. Tolkien")
 book_1.checkout_book()
 book_1.return_book()
-        
+# NOW WE SAVE THE BOOKS USING FILES
+my_book = Book("The Fellowship of the Ring", "J.R.R. Tolkien")
+with open("current_book.txt", "w") as file:
+    file.write(my_book.title)
+with open("current_book.txt", "r") as file:
+    books = file.read()
+    print(books)
